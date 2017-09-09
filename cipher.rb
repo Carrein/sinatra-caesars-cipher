c5b654eee53fb170a1a599192e906af29ec87364
Addison Ho <addisonho@outlook.com>
2017-09-09 23:40:11 +0800

    Completed Caesar's Cipher logic.

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'sinatra\reloader'
 
+encode = ""
+
 get '/' do
-    erb :index
+    encode = caesar_cipher(params['word'], params['shift'])
+    erb :index, :locals => {:encode => encode}
+end
+
+def caesar_cipher(cipher,factor)
+    factor = factor.to_i
+    words = cipher.split("")
+    words.each do |i|
+        if i.match(/[a-zA-Z]/)
+            factor.times do
+                i.next!
+            end
+        end
+    end
+    words.join()
 end
